42bdbb8e322c5147858fd7fc42515b6269164e1b
MeryAmun <chebesiawah@gmail.com>
2021-12-08 05:29:42 +0100

    created and connected alan api with react app,first test successful

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
+import alanBtn from '@alan-ai/alan-sdk-web'
+
+const alanKey =
+  '2fa4db26e51eefb4b9b150b7794734d62e956eca572e1d8b807a3e2338fdd0dc/stage'
 export const App = () => {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if (command === 'testCommand') {
+          alert('this code was executed')
+        }
+      },
+    })
+  }, [])
+
   return (
     <div>
-      <h1>Alan AI news App</h1>
+      <h1>Alan AI App</h1>
     </div>
   )
 }
